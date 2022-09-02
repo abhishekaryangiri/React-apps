4c13a844025de118bd9d0ec200cc4bec5aaea4e5
AbhishekAryanWeb170901 <bittoogoswami@gmail.com>
2022-09-02 17:42:08 +0530

    let const

---
+++ 12 let and const/let&const.js
@@ -0,0 +1,42 @@
+// const => for constant value
+const printMyName = (name) => {
+    // another method
+    // const printMyName = name => {
+    console.log(name)
+}
+printMyName('Abhi')
+
+
+
+const PrintNameAge = (name, age) => {
+    console.log(name, age);
+};
+PrintNameAge("abhi", 28)
+
+
+
+
+const multiply = (number) => {
+    return number*2
+    //shortcut of line 19-20
+    // const multiply = (number) => return number * 2;
+
+}
+console.log(multiply(8))
+
+
+
+
+
+
+// var 
+var myName = "Abhi";
+console.log(myName)
+
+
+myName ="Abhi"
+console.log(myName)
+
+
+
+// let => for variable values
