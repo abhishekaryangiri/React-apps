fc279be369dfb3fc1453814a39bf9f7d3d396ccb
AbhishekAryanWeb170901 <bittoogoswami@gmail.com>
2023-08-03 13:10:45 +0530

    abhiblog

---
+++ abhi-blog/src/App.js
@@ -0,0 +1,14 @@
+import { Button } from 'reactstrap';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to landing page</h1>
+      <Button>Submit here </Button>
+    </div>
+  );
+}
+
+export default App;
